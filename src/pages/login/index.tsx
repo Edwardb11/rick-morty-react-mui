4deeeeca616f3d7ab33abca573933fa478eb4d40
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-26 13:30:49 -0400

    feat:Catch data

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,8 +7,26 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-
+import { useState } from "react";
+import React from 'react';
+type LoginType = {
+  username: string;
+  password: string;
+};
 export const LoginPage: React.FC<{}> = () => {
+  const [loginData, setLoginData] = useState<LoginType>({
+    username: "",
+    password: "",
+  });
+
+  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit =(e: React.FormEvent<HTMLInputElement>)=>{
+    e.preventDefault()
+    console.log(loginData);
+  }
   return (
     <Container maxWidth="sm">
       <Grid
@@ -19,11 +37,36 @@ export const LoginPage: React.FC<{}> = () => {
         sx={{ minHeight: "100vh" }}>
         <Grid item>
           <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
-            <Typography variant="h4"  sx={{ mt: 1, mb: 1 }}>Iniciar Sesión</Typography>
-            <Box component="form">
-              <TextField margin="normal" fullWidth label="Email" sx={{ mt: 2, mb: 1.5 }} required />
-              <TextField margin="normal"  fullWidth label="Password" sx={{ mt: 1.5, mb: 1.5 }} required />
-              <Button fullWidth type="submit" variant="contained" sx={{ mt: 1.5, mb: 2 }} >
+            <Typography variant="h4" sx={{ mt: 1, mb: 1 }}>
+              Iniciar Sesión
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit}>
+              <TextField
+                margin="normal"
+                type="email"
+                name="username"
+                fullWidth
+                label="Email"
+                sx={{ mt: 2, mb: 1.5 }}
+                required
+                onChange={dataLogin}
+              />
+              <TextField
+                margin="normal"
+                type="password"
+                name="password"
+                fullWidth
+                label="Password"
+                sx={{ mt: 1.5, mb: 1.5 }}
+                required
+                onChange={dataLogin}
+
+              />
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                sx={{ mt: 1.5, mb: 2 }}>
                 Iniciar Sesión
               </Button>
             </Box>
