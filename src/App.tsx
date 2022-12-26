20939cd4419d2059e05b19fcb7336fae9e9013a3
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-26 10:57:14 -0400

    feat: NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Button, Container } from '@mui/material';
+import React from "react";
+import { Button, Container } from "@mui/material";
+import { NavBar } from "./common/NavBar";
 
 function App() {
   return (
-   <Container maxWidth='xl'>
-     <Button fullWidth variant='contained'>Hola mundo</Button>
-   </Container>
+    <div>
+      <NavBar />
+      <Container sx={{mt:9}} maxWidth="xl">
+        <Button fullWidth variant="contained">
+          Hola mundo
+        </Button>
+      </Container>
+    </div>
   );
 }
 
---
+++ src/common/NavBar.tsx
@@ -0,0 +1,41 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+export const NavBar: React.FC<{}> = () => {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Container maxWidth="xl">
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center">
+                <Grid item>
+                  {" "}
+                  <Typography> Logo</Typography>{" "}
+                </Grid>
+                <Grid item>
+                  <Stack direction="row" spacing={2}>
+                    <Button variant="contained">Login</Button>{" "}
+                    <Button variant="outlined">Register</Button>{" "}
+                  </Stack>
+                </Grid>
+              </Grid>
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+};
